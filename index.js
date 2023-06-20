8fdb64571283671e262507352fd204e79954f8e8
serhatprogramming <serhatprogramming@gmail.com>
2023-06-20 08:56:13 -0400

    delete and update added.

--- index.js
+++ index.js
@@ -9,28 +9,55 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
+// Custom Middleware
+const requestLogger = (req, res, next) => {
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request Path: ${req.path}`);
+  Object.keys(req.body).length !== 0 && console.log(`Request Body:`, req.body);
+  console.log("--------------------------------");
+  next();
+};
+// Implementing the Middleware
+app.use(requestLogger);
+// error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error("error message:", error.message);
+  if (error.name === "CastError") {
+    return response.status(400).json({ error: "invalid id" });
+  }
+  next(error);
+};
 
 app.get("/api/movies", async (req, res) => {
   const movies = await Movie.find({});
   res.json(movies);
 });
 
-app.get("/api/movies/:id", async (req, res) => {
-  const movie = await Movie.findById(req.params.id);
-  if (!movie) {
-    res.status(404).json({ error: "Movie not found" });
-  } else {
-    res.json(movie);
+app.get("/api/movies/:id", async (req, res, next) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      res.status(404).json({ error: "Movie not found" });
+    } else {
+      res.json(movie);
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-app.delete("/api/movies/:id", (req, res) => {
-  const movie = movies.find((movie) => movie.id === Number(req.params.id));
-  if (!movie) {
-    res.status(404).json({ error: "Movie not found" });
-  } else {
-    movies = movies.filter((movie) => movie.id !== Number(req.params.id));
-    res.status(200).json({ message: "Movie deleted successfully" });
+app.delete("/api/movies/:id", async (req, res, next) => {
+  try {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+    if (movie) {
+      res
+        .status(200)
+        .json({ message: `The movie [${movie.title}] removed successfully` });
+    } else {
+      res.status(404).json({ error: "The movie not found." });
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
@@ -39,11 +66,27 @@ app.post("/api/movies", async (req, res) => {
   if (!title) {
     return res.status(400).json({ error: "Title is required" });
   }
-  console.log("watchList: ", watchList); //
   const movie = new Movie({ title: title, watchList: watchList || false });
   const savedMovie = await movie.save();
   res.json(savedMovie);
 });
+
+app.put("/api/movies/:id", async (req, res, next) => {
+  const { title, watchList } = req.body;
+  try {
+    const updatedMovie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      { title, watchList },
+      { new: true }
+    );
+    res.json(updatedMovie);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// implement error handler middleware
+app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
